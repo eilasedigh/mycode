ad8b4493faeae6f85f55fbdcabb9cc8687ba220b
eilasedigh <Eila.Sedigh@gmail.com>
2019-10-14 15:45:51 -0400

    day1

---
+++ challengelab02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env/python3
+col_list= ["blue", "Columbus"]
+col_list.append(1492)
+print(col_list)
+
+username=input(" What is your name? ")
+
+print(f"In {col_list[2]}, {col_list[1]} sailed the ocean blue. {username} fell of the boat.")
---
+++ challengelab03.py
@@ -0,0 +1,5 @@
+#!usr/bin/env/python3
+
+numbers=[1, 2, 3, ["a", "b", "c", [1, 4, 7, 10]]]
+
+print(numbers[3][3][3])
